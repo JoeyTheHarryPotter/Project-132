59530f3a81b80ff2bc61ae37b3c32c6ee77a6e9d
JoeyTheHarryPotter <74834315+JoeyTheHarryPotter@users.noreply.github.com>
2021-07-15 20:18:39 -0400

    Add files via upload

---
+++ Calender.js
@@ -0,0 +1,35 @@
+img = "";
+status = "";
+
+function preload(){
+    img = loadImage('Calender.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640, 640);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 640);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
